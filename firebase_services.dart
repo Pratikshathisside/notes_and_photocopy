db7c59020ea46694ba33cf1331445126e3eabbcd
Pratiksha Goswami <104529563+Pratikshathisside@users.noreply.github.com>
2023-03-02 17:40:09 +0530

    Create firebase_services.dart

---
+++ firebase_services.dart
@@ -0,0 +1,34 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseServices {
+  final _auth = FirebaseAuth.instance;
+  final _googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+  GoogleSignInAccount get user => _user!;
+
+  signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser =
+        await GoogleSignIn(scopes: <String>["email"]).signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  //Sign out
+  signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+}
